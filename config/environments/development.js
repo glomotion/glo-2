2a391c4ebb16acc1b541a78786a21ffacd865d12
Tim Paul <tim@glo.id.au>
2016-01-19 19:15:51 +1100

    initial commit for the new build - based off a minimal webpack instance

--- config/environments/development.js
+++
@@ -1,3 +0,0 @@
-export default {
-  identityProperty: 'APP_IDENTITY',
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,36 @@
-import path from "path";
-import webpack from "webpack";
-import ExtractTextPlugin from "extract-text-webpack-plugin";
+var path = require('path');
+var webpack = require('webpack');
 
-
-export default (DEBUG, PATH, PORT=3000) => ({
-  entry: (DEBUG ? [
-    `webpack-dev-server/client?http://localhost:${PORT}`,
-  ] : []).concat([
-    './src/main.less',
+module.exports = {
+  entry: [
     'babel-polyfill',
+    './src/theme/main.less',
     './src/main',
-  ]),
-
+    'webpack-dev-server/client?http://localhost:8080'
+  ],
   output: {
-    path: path.resolve(__dirname, PATH, "generated"),
-    filename: DEBUG ? "main.js" : "main-[hash].js",
-    publicPath: "/generated/"
+      publicPath: '/',
+      filename: 'main.js'
   },
-
-  cache: DEBUG,
-  debug: DEBUG,
-
-  // For options, see http://webpack.github.io/docs/configuration.html#devtool
-  devtool: DEBUG && "eval",
-
+  debug: true,
+  devtool: 'source-map',
   module: {
     loaders: [
-      // Load ES6/JSX
-      { test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, "src"),
-        ],
-        loader: "babel-loader",
+      { 
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
         query: {
-          plugins: ['transform-runtime'],
-          presets: ['es2015', 'stage-0', 'react'],
+          presets: ['es2015']
         }
       },
-
-      // Load styles
-      { test: /\.less$/,
-        loader: DEBUG
-          ? "style!css!autoprefixer!less"
-          : ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+      { 
+        test: /\.less$/,
+        loader: "style!css!autoprefixer!less"
       },
-
-      // Load images
-      { test: /\.jpg/, loader: "url-loader?limit=10000&mimetype=image/jpg" },
-      { test: /\.gif/, loader: "url-loader?limit=10000&mimetype=image/gif" },
-      { test: /\.png/, loader: "url-loader?limit=10000&mimetype=image/png" },
-      { test: /\.svg/, loader: "url-loader?limit=10000&mimetype=image/svg" },
-
-      // Load fonts
-      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
     ]
   },
-
-  plugins: DEBUG
-    ? []
-    : [
-      new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-      new ExtractTextPlugin("style.css", {allChunks: false}),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {screw_ie8: true, keep_fnames: true, warnings: false},
-        mangle: {screw_ie8: true, keep_fnames: true}
-      }),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.AggressiveMergingPlugin(),
-    ],
-
-  resolveLoader: {
-    root: path.join(__dirname, "node_modules"),
-  },
-
-  resolve: {
-    root: path.join(__dirname, "node_modules"),
-
-    modulesDirectories: ['node_modules'],
-
-    alias: {
-      environment: DEBUG
-        ? path.resolve(__dirname, 'config', 'environments', 'development.js')
-        : path.resolve(__dirname, 'config', 'environments', 'production.js')
-    },
-
-    // Allow to omit extensions when requiring these files
-    extensions: ["", ".js", ".jsx"],
+  devServer: {
+    contentBase: "./src"
   }
-});
+};
